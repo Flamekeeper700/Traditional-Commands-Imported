b03127f23e892209364d5328b2278e234c7e0f25
Brody <fekrsnhd@gmail.com>
2026-02-07 11:51:18 -0500

    init 2026

---
+++ src/main/java/frc/robot/subsystems/CANRollerSubsystem.java
@@ -0,0 +1,50 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.RollerConstants;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+
+/** Class to run the rollers over CAN */
+public class CANRollerSubsystem extends SubsystemBase {
+  private final PWMSparkMax rollerMotor;
+private final SparkMaxConfig rollerConfig;
+  public CANRollerSubsystem() {
+    rollerConfig = new SparkMaxConfig();
+    // Set up the roller motor as a brushed motor
+    rollerMotor = new PWMSparkMax(RollerConstants.ROLLER_MOTOR_ID);
+
+    // Set can timeout. Because this project only sets parameters once on
+    // construction, the timeout can be long without blocking robot operation. Code
+    // which sets or gets parameters during operation may need a shorter timeout.
+    //rollerMotor.setCANTimeout(250);
+
+    // Create and apply configuration for roller motor. Voltage compensation helps
+    // the roller behave the same as the battery
+    // voltage dips. The current limit helps prevent breaker trips or burning out
+    // the motor in the event the roller stalls.
+    
+    rollerConfig.voltageCompensation(RollerConstants.ROLLER_MOTOR_VOLTAGE_COMP);
+    rollerConfig.smartCurrentLimit(RollerConstants.ROLLER_MOTOR_CURRENT_LIMIT);
+    //rollerMotor.configure(rollerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
+
+  @Override
+  public void periodic() {
+  }
+
+  /** This is a method that makes the roller spin */
+  public void runRoller(double forward) {
+    rollerMotor.set(forward);
+  }
+
+}
